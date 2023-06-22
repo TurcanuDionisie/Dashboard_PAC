112409630bcb723815a6b19eaf4563328598df6f
marzowill96 <willymarzo96@gmail.com>
2023-06-22 16:07:46 +0200

    a

--- codici/engine_tool_pac.py
+++ codici/engine_tool_pac.py
@@ -32,6 +32,55 @@ isin_name_dict = {alfa.iloc[i,1] : alfa.iloc[i,0] for i in range(len(alfa))}
 quote.columns = quote.iloc[1]
 quote = quote.iloc[2:]
 
-#%%
+#%% AGGIORNO DATI-...... DA FINIRE
+# directory_universi = r'I:\Documenti\File PMC\In Corso'
+
+# ch = pd.read_excel(directory_universi +'\par - universo ch mif sintesi.xlsx', sheet_name='Q.ta Pubblicata')
+# mbb = pd.read_excel(directory_universi + '\par - universo mbb mif sintesi.xlsx', sheet_name='Q.ta Pubblicata')
+# mif = pd.read_excel(directory_universi + '/a&p - universo mgf italiani.xlsx', sheet_name='Quota Pubb Rettificata')
+
+# # FILE ADJUSTMENTS
+# ch = ch.iloc[1:] 
+# ch.iloc[0,0] = 'Date' 
+# ch.columns = ch.loc[1]
+# ch = ch.iloc[1:].set_index('Date') 
+# for s in ch.columns:
+#     ch[s] = pd.to_numeric(ch[s])
+# ch = ch.replace(0,np.nan)
+# ch.fillna(method='ffill', inplace=True)
+
+# mbb = mbb.iloc[1:] 
+# mbb.iloc[0,0] = 'Date' 
+# mbb.columns = mbb.loc[1]
+# mbb = mbb.iloc[1:].set_index('Date') 
+# for s in mbb.columns:
+#     mbb[s] = pd.to_numeric(mbb[s])
+# mbb = mbb.replace(0,np.nan)
+# mbb.fillna(method='ffill', inplace=True)
+
+# mif = mif.iloc[2:] 
+# mif = mif.rename(columns={mif.columns[0] : 'Date'}) 
+# mif = mif.iloc[1:].set_index('Date') 
+# for s in mif.columns:
+#     mif[s] = pd.to_numeric(mif[s])
+# mif = mif.replace(0,np.nan)
+# mif.fillna(method='ffill', inplace=True)
 
 
+# isin = quote.columns[0]
+
+# quote.index = quote.index.append(ch[ch.index > quote.index[-1]].index)
+
+# for isin in quote.columns:
+    
+#     if isin.startswith('IE') and isin_name_dict[isin].startswith('Challenge'):
+#         quote[isin] = quote[isin].append(ch[isin][ch.index > quote.index[-1]].T)
+        
+#     elif isin.startswith('IE') and isin_name_dict[isin].startswith('Mediolanum'):
+#         quote[isin].append(mbb[isin][mbb.index > quote.index[-1]])
+        
+# else:
+#     quote[isin].append(mif[isin][mif.index > quote.index[-1]])
+        
+#%%         
+        
